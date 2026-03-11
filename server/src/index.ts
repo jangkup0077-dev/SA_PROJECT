a9dd6636a65b09f9cda8a593041f7ff95ba6daa7
nattaphat010447 <jangkup90@gmail.com>
2026-03-11 15:29:06 +0700

    update

--- server/src/index.ts
+++ server/src/index.ts
@@ -32,6 +32,10 @@ export const io = new Server(httpServer, {
 
 io.on('connection', (socket) => {
   console.log('⚡ User connected:', socket.id);
+  
+  socket.on('join_global', (userId) => {
+    socket.join(`user_${userId}`);
+  });
 
   socket.on('join_match', (matchId) => {
     socket.join(`match_${matchId}`);
